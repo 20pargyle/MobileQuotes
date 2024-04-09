633976545cf3c45eab2c8c639b783f7459eef02e
Parker Argyle <parkerargyle@icloud.com>
2024-04-08 18:08:00 -0600

    functional query and error div - only logs the promised list

--- quotes.js
+++ quotes.js
@@ -1,5 +1,7 @@
 const initialQuote = document.getElementById("initial-quote");
 const initialAuthor = document.getElementById("initial-author");
+const authorSearch = document.getElementById("search-bar");
+const errorDiv = document.getElementById("error-display");
 
 async function getRandomQuote(elQuote, elAuthor) {
     const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
@@ -9,4 +11,24 @@ async function getRandomQuote(elQuote, elAuthor) {
     elAuthor.innerText = `- ${quoteObj["author"]}`;
 }
 
+async function search(userQuery) {
+    const result1 = await fetch(`https:usu-quotes-mimic.vercel.app/api/search?query=${userQuery}`);
+    const result2 = await result1;
+    const quoteList = await result1.json();
+    return quoteList;
+}
+
 getRandomQuote(initialQuote, initialAuthor);
+
+authorSearch.addEventListener("keydown", function(e) {
+    if (e.key === "Enter"){
+        if (authorSearch.value == ""){
+            errorDiv.innerText = "Empty string!";
+        }
+        // add an else-if for any other conditions here
+        else {
+            const quoteList = search(authorSearch.value);
+            console.log(quoteList);
+        }
+    }
+});
